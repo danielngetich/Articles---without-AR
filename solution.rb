6c0e0f74a4a694c468f6c04f458f2c2511e9a55b
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-10-31 18:41:42 +0300

    add more methods

--- solution.rb
+++ solution.rb
@@ -52,4 +52,19 @@ class Magazine
         article.magazine.name==@name}
         .map{|article|article.author}.uniq
       end
+      def self.find_by_name(name)
+        Magazine.all.find{|magazine|
+        magazine.name==name}
+      end
+      def article_titles
+        Article.all.filter{|article|
+          article.magazine.name==@name}
+          .map{|article|article.title}
+      end
+      # def contributing_authors
+      #   Article.all.filter{|article|
+      #     article.magazine.name==@name}
+      #     .
+      # end
 end
+  
