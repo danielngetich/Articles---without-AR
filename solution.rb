e889729f9faf455fe5cb4671ed0ec918400cdc1a
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-10-31 11:28:39 +0300

    add methods

--- solution.rb
+++ solution.rb
@@ -1,8 +1,7 @@
-require "pry"
 class Article
-    attr_accessor :author, :magazine, :title
+    attr_reader :magazine, :title
         @@all=[]
-    def initialize(author:, magazine:, title:)
+    def initialize(author, magazine, title)
         @author=author
         @magazine=magazine
         @title=title
@@ -11,24 +10,46 @@ class Article
     def self.all
         @@all
     end
+    def author
+        @author.name
     end
+end
 
-    class Author<Article
-        def initialize name
-          @name=name
+class Author
+    attr_reader :name
+    def initialize name
+      @name=name
+    end
+    def article
+        Article.all.filter do |article|
+          article.author==@name
         end
     end
-        
-    class Magazine<Article
-        @@all=[]
-          attr_accessor :name,:category
-          def initialize(name,category)
-               @name=name
-               @category=category
-               @@all<<self
-          end
-          def self.all
-            @@all
-          end
+    def magazines
+      articles.map{|article| article.magazine}.uniq
     end
-binding.pry
+    def add_article(magazine,title)
+      Article.new(self,magazine,title)
+    end
+    def topic_areas
+      magazines.map{|magazine|magazine.category}.uniq
+    end
+  end
+        
+  class Magazine
+    @@all=[]
+      attr_accessor :name,:category
+      def initialize(name,category)
+           @name=name
+           @category=category
+           @@all<<self
+      end
+      def self.all
+        @@all
+      end
+      def contributors
+        Article.all.filter{|article|
+        article.magazine.name==@name}
+        .map{|article|article.author}.uniq
+      end
+  end
